e5711e2f9fb7af6fbb8d892195843e7c754fc76d
Ben Galewsky <ben@peartreestudio.net>
2022-12-21 15:39:26 -0600

    Update describe model to include the pipeline

--- describeModel.py
+++ describeModel.py
@@ -1,6 +1,6 @@
-import json
-
-from garden_ai import GardenClient
+import mlflow
+from garden_ai import GardenClient, step, Pipeline
+from pymatgen.core import Composition
 
 client = GardenClient()
 garden = client.create_garden(authors=['Ward, Logan'],
@@ -11,3 +11,40 @@ garden.doi = '10.1038/npjcompumats.2016.28'
 
 client.register_metadata(garden)
 
+
+@step
+def parse_compositions(formulas: list[str]) -> list[Composition]:
+    return [Composition(x) for x in formulas]
+
+
+@step
+def featurize_composition(compositions: list[Composition]) -> list:
+    model_name = "Matminer Featurizer"
+    model_version = "Production"
+    logged_featurizer = f"models:/{model_name}/{model_version}"
+
+    # Load featurizer as a pyfunc model
+    loaded_featurizer = mlflow.pyfunc.load_model(logged_featurizer)
+    return loaded_featurizer.predict(compositions)
+
+
+@step
+def predict(x: list) -> list[float]:
+    model_name = "Matminer"
+    model_version = "Production"
+    logged_model = f"models:/{model_name}/{model_version}"
+
+    # Load model as a SciKitLearn Model.
+    loaded_model = mlflow.sklearn.load_model(logged_model)
+    return loaded_model.predict(x)
+
+
+predictor_pipeline = Pipeline(
+    title="Formation enthalpy predictor from molecule compositions",
+    steps=(parse_compositions, featurize_composition, predict),
+    authors=garden.authors,
+)
+
+garden.pipelines = [predictor_pipeline]
+
+print(predictor_pipeline(["NaCl"]))
